fbaafacd22c0aca23ffcfeb8b8de483f42df4d41
Meni Yakove <myakove@gmail.com>
2023-06-02 18:40:00 +0300

    Fix dockerfile and make item in db unique

--- Dockerfile
+++ Dockerfile
@@ -4,21 +4,20 @@ EXPOSE 5000
 ENV PATH="/root/.local/bin:$PATH"
 RUN set -x \
     && dnf -y update \
-    && dnf -y install python3.8 python3.9 python3.10 python3.11 python3-pip git hub podman \
+    && dnf -y install python3-pip \
     && dnf clean all \
     && rm -rf /var/cache/yum
 
-COPY . /github-webhook-server
-WORKDIR /github-webhook-server
+COPY . /app
+WORKDIR /app
 RUN ln -s /usr/bin/python3 /usr/bin/python \
     && curl -sSL https://install.python-poetry.org | python3 - \
     && poetry --version \
     && python3 -m pip install pip --upgrade \
-    && python3 -m pip install pipx importlib \
     && poetry config cache-dir /app \
     && poetry config virtualenvs.in-project true \
     && poetry config installer.max-workers 10 \
     && poetry config --list \
     && poetry install
 
-ENTRYPOINT ["poetry", "run", "python3", "webhook_server_container/app.py"]
+ENTRYPOINT ["poetry", "run", "python3", "app/app.py"]
--- app/app.py
+++ app/app.py
@@ -1,22 +1,39 @@
 import json
+import os
 import sqlite3
 
 from flask import Flask, jsonify, request
 
 
+DB_BASH_PATH = os.environ.get("TODO_DB_PATH", "/data")
+DB_PATH = os.path.join(DB_BASH_PATH, "todo-list.db")
+TABLE_NAME = "ToDoList"
 app = Flask("ToDoList")
+
+
+def get_db():
+    conn = sqlite3.connect(DB_PATH)
+    conn.execute(
+        f"CREATE TABLE if not exists {TABLE_NAME} (ITEM TEXT NOT NULL, CONSTRAINT name_unique UNIQUE (ITEM));"
+    )
+    return conn
 
 
 def add_item(item):
     app.logger.info(f"Adding {item}")
-    table_name = "ToDoList"
-    conn = sqlite3.connect("todo-list.db")
-    conn.execute(f"CREATE TABLE if not exists {table_name} (ITEM TEXT NOT NULL);")
-    conn.execute(f"INSERT INTO {table_name} (ITEM) VALUES ('{item}');")
+    conn = get_db()
+    conn.execute(f"INSERT INTO {TABLE_NAME} (ITEM) VALUES ('{item}');")
     conn.commit()
 
 
-@app.route("/set", methods=["POST"])
+def delete_item(item):
+    app.logger.info(f"Delete {item}")
+    conn = get_db()
+    conn.execute(f"DELETE FROM {TABLE_NAME} WHERE ITEM = '{item}';")
+    conn.commit()
+
+
+@app.route("/add", methods=["POST"])
 def set_to_list():
     try:
         data = request.json
@@ -32,13 +49,25 @@ def set_to_list():
 def get_list():
     try:
         res = {"items": [], "status": True}
-        conn = sqlite3.connect("todo-list.db")
-        _data = conn.execute("select * from ToDoList")
+        conn = get_db()
+        _data = conn.execute(f"select * from {TABLE_NAME}")
         for row in _data:
             res["items"].append(row[0])
 
         response = jsonify(res)
         return response
+    except Exception as ex:
+        app.logger.error(f"Error: {ex}")
+        return json.dumps({"status": False})
+
+
+@app.route("/del", methods=["POST"])
+def delete_from_list():
+    try:
+        data = request.json
+        delete_item(data["item"])
+        data["status"] = True
+        return data
     except Exception as ex:
         app.logger.error(f"Error: {ex}")
         return json.dumps({"status": False})
